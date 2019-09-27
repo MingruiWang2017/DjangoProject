0ccf7ca4c527c6ab220624d8e9658e2866cd51ed
王铭锐 <wangmingrui@inspur.com>
2019-09-27 09:38:42 +0800

    手动实现表单传递信息

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.http import Http404
+from django.contrib.auth.models import User
 
-from .models import Board
+from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 # Create your views here.
 
@@ -33,3 +35,27 @@ def board_topics(request, pk):
     #     raise Http404
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  # TODO: 临时使用一个账号作为登录用户
+
+        topic = Topic.objects.create(  # 创建新的Topic
+            subject = subject,
+            board = board,
+            starter = user
+        )
+        post = Post.objects.create(
+            message = message,
+            topic = topic,
+            created_by = user
+        )
+        return redirect('board_topics', pk=board.pk)  # TODO: 重定向到创建出的Topic页面
+
+    return render(request, 'new_topic.html', {'board':board})
